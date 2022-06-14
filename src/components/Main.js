f00f0fd5ea3258491b0f293073e60bcc03403d05
Cian Butler <ciandevelops@gmail.com>
2022-06-14 13:39:29 -0400

    row set up

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ const Main = () => {
       setMovies(response.data.results);
     });
   }, []);
-  console.log(movie);
+  
 
   //   const truncateString = (str, num) => {
   //     if (str.length > num) {
