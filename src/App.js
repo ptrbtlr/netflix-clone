eb4f4de791db595de90a4cf613da355ac2f4b3a2
Cian Butler <ciandevelops@gmail.com>
2022-06-14 10:54:34 -0400

    react router set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
